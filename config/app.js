8ee6178f1dc2f846ff59a19720dc2b86ce47b64d
Oliver Otchere <oliverotchere4@gmail.com>
2021-07-17 12:57:18 +0000

    added app level config

---
+++ config/app.js
@@ -0,0 +1,5 @@
+// export const BACKEND_URI=process.env.BACKEND_URI
+// export const NEXT_BACKEND_URI=process.env.NEXT_BACKEND_URI
+
+export const BACKEND_URI="http://localhost:8000"
+export const NEXT_BACKEND_URI='http://localhost:3000/api'
